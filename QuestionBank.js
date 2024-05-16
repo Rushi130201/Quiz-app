0488598ec73e13b6c397fd51decb09775dc19b8b
Rushi130201 <159525780+Rushi130201@users.noreply.github.com>
2024-05-16 02:07:33 -0700

    Add files via upload

---
+++ QuestionBank.js
@@ -0,0 +1,34 @@
+const qBank = [
+	{
+		id: 1,
+		question: "Which of the following is used in React.js to increase performance?",
+		options: ["A) Virtual DOM", "B) Original DOM", "C) Both A and B", "D) None of the above"],
+		answer: "A) Virtual DOM",
+	},
+	{
+		id: 2,
+		question: "What is ReactJS?",
+		options: ["A) Server-side framwork", "B) user-interface framework", "C) Both A and B", "D)  None of the above"],
+		answer: "B) user-interface framework",
+	},
+	{
+		id: 3,
+		question: "Identify the one which is used to pass data to components from outside",
+		options: ["A) Render with arguments", "B) setState", "C) props", "D) PropTypes"],
+		answer: "C) props"
+	},
+	{
+		id: 4,
+		question: "Who created ReactJS?",
+		options: ["A) Jordan walke", "B) Jordan mike", "C) Tim lee", "D) Jordan lee"],
+		answer: "A) Jordan walke"
+	},
+	{
+		id: 5,
+		question: "In which language is React.js written?",
+		options: ["A) Java", "B) Javascript", "C) Python", "D) PHP"],
+		answer: "B) Javascript"
+	},
+]
+
+export default qBank;
